8f23b23b297b30c664d9f9637f1db6f9abf3f7ab
홍석현 <114850357+fr35wo@users.noreply.github.com>
2023-09-17 00:31:44 +0900

    Add files via upload

--- string1.js
+++ string1.js
@@ -53,3 +53,49 @@ for (let path of a1) {
   const nameWithoutExtension = fileName.split('.').shift(); // 파일 이름 부분 추출
   console.log(`파일 이름 부분: ${nameWithoutExtension}`);
 }
+
+//(1) 빈 배열을 만든다.
+//(2) Math.random() 메소드를 사용하여, 1 이상 100 이하의 정수 난수를 100개 생성해서, 배열에 채운다.
+//(3) 배열을 오름차순으로 정렬한다.
+//(4) 배열을 출력한다.
+
+let a2 = [];
+for(let i=0; i<100; i++){
+  a3 = Math.floor(Math.random()*100 +1 );
+  a2.push(a3);
+  a2.sort(compareNumber);
+}
+
+function compareNumber(i,j){
+  return i - j;
+}
+
+console.log(a2);
+
+//(1) 빈 배열을 만든다.
+//(2) Math.random() 메소드를 사용하여, 1 이상 100 이하의 정수 난수를 100개 생성해서, 배열에 채운다.
+//(3) 배열에 원소들 중에서 짝수(2의 배수)를 찾아서 모두 제거한다.
+//(4) 배열을 출력한다.
+
+let a4 = [];
+for(let i=0; i<100; i++){
+  a5 = Math.floor(Math.random()*100 +1 );
+  if(a5%2 == 1){
+    a4.push(a5);
+  }
+}
+console.log(a4);
+
+
+
+//2. typeof - 값의 타입 확인 가능
+
+//3. 
+//sort 정렬메소드 이상하게됨
+//reverse 배열의 항목 순서 뒤집기
+
+//4.  
+//indexOf 문자열에서 부분_문자열을 찾아서, 찾은 위치(index)를 리턴한다.
+//lastIndexOf 시작_위치에서부터 거꾸로(앞으로) 찾기 시작한다.
+//search 문자열에서 정규식을 찾아서, 찾은 위치(index)를 리턴
+//
