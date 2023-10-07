dfd2f791ff39592ac9f9d5b110db69316840c515
홍석현 <114850357+fr35wo@users.noreply.github.com>
2023-10-07 21:59:07 +0900

    Add files via upload

---
+++ exam8.js
@@ -0,0 +1,13 @@
+function countSpace(s) { 
+    let spaceCount = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            spaceCount++;
+        }
+    }
+    
+    return spaceCount;
+} 
+console.log(countSpace("aa bb ccc")); 
+console.log(countSpace("a b cc ddd  e"));
