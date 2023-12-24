30de3a89544e761f31a7b10323e3aba38c8814cf
seokhyeon <114850357+fr35wo@users.noreply.github.com>
2023-12-25 01:49:25 +0900

    Add files via upload

---
+++ vue_app4/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="App">
+  <select v-model="color">
+    <option value="#ffe">yellow</option>
+    <option value="#efe">green</option>
+    <option value="#fee">red</option>
+    <option value="#eef">blue</option>
+  </select>
+  <p />
+  <input type="text" v-model="message" />
+  <hr />
+  <MyMessage :bgColor="color" :text="message" /> 
+</div>
+</template>
+
+<script>
+import MyMessage from './Mymessage.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      color: '#ffe',
+      message: 'Hello'
+    }
+  },
+  components: {
+    MyMessage
+  }
+}
+</script>
+
+<style>
+div#App{
+  border: 1px solid gray; padding: 20px;
+  box-shadow: 5px 5px 5px gray; 
+  width: 400px;
+}
+select {
+  padding: 4px; width: 100px;
+}
+input{
+  padding: 4px;
+}
+hr{
+  margin: 20px 0;
+}
+</style>
---
+++ vue_app4/src/Mymessage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="MyMessage" :style="{backgroundColor: bgColor}">
+            {{ text }}
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'MyMessage',
+    props: {
+        bgColor: String,
+        text: String
+    }
+}
+</script>
+
+<style scoped>
+div#Message {
+    border: 1px solid gray; width: 200px;
+    height: 150px; box-shadow: 5px 5px 5px gray;
+    font-size: 15pt; padding: 20px;
+}
+</style>
